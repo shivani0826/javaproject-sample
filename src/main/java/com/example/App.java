23380d1a64750316440368b01fd2441193389679
root <root@ip-172-31-17-155.ec2.internal>
2025-12-31 08:57:07 +0000

    Fix Java syntax error in App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,20 +1,9 @@
 package com.example;
 
 public class App {
-<<<<<<< HEAD
-    public static void main(String[] args) throws InterruptedException {
-        System.out.println("JavaProject CI/CD Pipeline Running 🚀");
 
-        while (true) {
-            Thread.sleep(60000); // sleep 60 seconds
-        }
-    }
-}
-
-=======
     public static void main(String[] args) {
-        System.out.println("JavaProject CI/CD Pipeline Running 🚀");
-        while(true) {}
+        System.out.println("Java Project running successfully via Jenkins and Docker!");
     }
+
 }
->>>>>>> 2acd55238bf28190644b832d75f6469d701c428a
